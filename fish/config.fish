c6c690950d1e56a4292a52b0f72250b92d586e99
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2025-12-06 20:08:56 -0300

    new fish alias and import

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,11 @@
 if status is-interactive
-# Commands to run in interactive sessions can go here
+    # Commands to run in interactive sessions can go here
 end
 
 # Start ssh-agent
 if not set -q SSH_AUTH_SOCK
     ssh-agent -c | source
 end
-
 
 fish_add_path ~/.local/bin
 set -x STARSHIP_CONFIG /Users/matheuscunha/.config/starship/starship.toml
@@ -15,7 +14,26 @@ starship init fish | source
 zoxide init fish | source
 
 # Aliases
-alias cat="bat --theme=$(defaults read -globalDomain AppleInterfaceStyle &> /dev/null && echo default || echo GitHub)"
 alias l="ls -la"
-alias ls 'eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions'
-alias python=python3
+alias ls='eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions'
+alias python="python3"
+alias ..="cd .."
+alias k="kubectl"
+alias g="git"
+alias gc="git commit"
+alias gca="git commit -a -m"
+alias n="nvim"
+alias ni="npm install"
+alias nd="npm run dev"
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+# go
+set -gx GOPATH "$HOME/go"
+fish_add_path "$GOPATH/bin"
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
